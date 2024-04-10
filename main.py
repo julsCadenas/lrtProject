b499a6c24023db6966947ff44651c81e3b5b72e3
julsCadenas <jscadenas12@gmail.com>
2024-04-10 21:55:42 +0800

    completed single journey page design

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ class IndexPage(CTkFrame):
         super().__init__(parent)
 
         self.configure(fg_color="#242424") #change frame background color
-        customFont = customtkinter.CTkFont(family="Century Gothic", size=28, weight="bold")
+        customFont = customtkinter.CTkFont(family="Century Gothic", size=20, weight="bold")
 
         # train background
         imageBackground = CTkImage(dark_image=Image.open("images/background.jpg"), size=(500,700))
@@ -45,13 +45,13 @@ class IndexPage(CTkFrame):
 
         # single journey button
         singleBtn = CTkButton(master=self, text="Single Journey Ticket", fg_color="#9966CC", hover_color="#A32CC4",
-                              corner_radius=32, font=customFont, width=300, height=50,
+                              corner_radius=15, font=customFont, width=300, height=40,
                               command=lambda: controller.showFrame(SingleJourneyPage))
-        singleBtn.place(relx=0.75, rely=0.47, anchor="center")
+        singleBtn.place(relx=0.75, rely=0.485, anchor="center")
 
         # stored value button
         storedBtn = CTkButton(master=self, text="Stored Value Ticket", fg_color="#9966CC", hover_color="#A32CC4",
-                              corner_radius=32, font=customFont, width=340, height=50)
+                              corner_radius=15, font=customFont, width=300, height=40)
         storedBtn.place(relx=0.75, rely=0.56, anchor="center")
 
 class SingleJourneyPage(CTkFrame):
@@ -74,53 +74,77 @@ class SingleJourneyPage(CTkFrame):
             13: "Recto"
         }
 
+        # Labels for station names
+        stationLabels = ["Recto", "Legarda", "Pureza", "V.Mapa", "J.Ruiz", "Gilmore", "Betty Go",
+            "Cubao", "Anonas", "Katipunan", "Santolan", "Marikina", "Antipolo"]
+
         def slideValue(value):
             slideLabel.configure(text=stationValues[int(value)])
 
         self.configure(fg_color="#242424") #change frame background color
-        customFont = customtkinter.CTkFont(family="Century Gothic", size=28, weight="bold")
+        customFont = customtkinter.CTkFont(family="Century Gothic", size=20, weight="bold")
+
+        mainFrame = CTkFrame(master=self, height=620, width=600, corner_radius=15)
+        mainFrame.place(relx=0.665, rely=0.5, anchor="center")
 
         titleFont = customtkinter.CTkFont(family="Century Gothic", size=45, weight="bold")
-        titleLabel2 = CTkLabel(self, text="Single Journey Ticket", font=titleFont)
-        titleLabel2.place(relx=0.72, rely=0.1, anchor="center")
+        titleLabel2 = CTkLabel(mainFrame, text="Single Journey Ticket", font=titleFont)
+        titleLabel2.place(relx=0.5, rely=0.1, anchor="center")
 
-        backBtn = CTkButton(master=self, text="Cancel", fg_color="#9966CC", hover_color="#A32CC4",
-                            corner_radius=15, font=customFont, width=150, height=50,
+        backBtn = CTkButton(master=mainFrame, text="Cancel", fg_color="#9966CC", hover_color="#A32CC4",
+                            corner_radius=10, font=customFont, width=150, height=35,
                             command=lambda: controller.showFrame(IndexPage))
-        backBtn.place(relx=0.7, rely=0.9, anchor="center")
+        backBtn.place(relx=0.37, rely=0.9, anchor="center")
 
-        confirmBtn = CTkButton(master=self, text="Confirm", fg_color="#9966CC", hover_color="#A32CC4",
-                          corner_radius=15, font=customFont, width=150, height=50,
+        confirmBtn = CTkButton(master=mainFrame, text="Confirm", fg_color="#9966CC", hover_color="#A32CC4",
+                          corner_radius=10, font=customFont, width=150, height=35,
                           command=lambda: controller.showFrame(IndexPage))
-        confirmBtn.place(relx=0.86, rely=0.9, anchor="center")
+        confirmBtn.place(relx=0.64, rely=0.9, anchor="center")
+
+        locationFont = customtkinter.CTkFont(family="Century Gothic", size=32, weight="bold")
+        locationTitle = customtkinter.CTkLabel(mainFrame, text="You Are Here:", font=locationFont)
+        locationTitle.place(relx=0.5, rely=0.24, anchor="center")
+
+        dropdownFont = customtkinter.CTkFont(family="Century Gothic", size=20, weight="bold")
+        locationDropdown = customtkinter.CTkComboBox(mainFrame, values=stationLabels, width=240, height=30, font=dropdownFont, justify="center")
+        locationDropdown.place(relx=0.5, rely=0.32, anchor="center")
 
         # frame to put slider into
-        sliderFrame = CTkFrame(master=self, height=620, width=250)
-        sliderFrame.place(relx=0.2, rely=0.5, anchor="center")
+        sliderFrame = CTkFrame(master=self, height=620, width=270, corner_radius=15)
+        sliderFrame.place(relx=0.18, rely=0.5, anchor="center")
 
         # station slider picker (inside the frame)
-        slider = CTkSlider(master=sliderFrame, from_=1, to=13, number_of_steps=12, orientation="vertical", button_color="#9966CC", height=610, width=30, command=slideValue)
+        slider = CTkSlider(master=sliderFrame, from_=1, to=13, number_of_steps=12, orientation="vertical",
+                           button_color="#9966CC", height=610, width=30, command=slideValue, progress_color="#4a4d50", button_hover_color="#A32CC4")
         slider.place(relx=0.2, rely=0.5, anchor="center")
         slider.set(0) # set slider to 1/bottom
 
-        destinationTitle = customtkinter.CTkLabel(self, text="Destination", font=customFont)
-        destinationTitle.place(relx=0.72, rely=0.42, anchor="center")
+        destinationFont = customtkinter.CTkFont(family="Century Gothic", size=32, weight="bold")
+        destinationTitle = customtkinter.CTkLabel(mainFrame, text="Destination:", font=destinationFont)
+        destinationTitle.place(relx=0.5, rely=0.42, anchor="center")
 
-        slideLabel = customtkinter.CTkLabel(self, text="", font=customFont)
-        slideLabel.place(relx=0.72, rely=0.5, anchor="center")
+        sliderLabelFrame = CTkFrame(mainFrame, height=50, width=250, corner_radius=15, border_color="#4a4d50")
+        sliderLabelFrame.place(relx=0.5, rely=0.5, anchor="center")
+
+        slideLabelFont = customtkinter.CTkFont(family="Century Gothic", size=24, weight="bold")
+        slideLabel = customtkinter.CTkLabel(sliderLabelFrame, text="", font=slideLabelFont)
+        slideLabel.place(relx=0.5, rely=0.5, anchor="center")
 
         customFont2 = customtkinter.CTkFont(family="Century Gothic", size=20, weight="bold")
 
         # Calculate the position of each label
         labelPositions = [(0.035 + i * 0.078) for i in range(13)]
 
-        # Labels for station names
-        stationLabels = ["Recto", "Legarda", "Pureza", "V.Mapa", "J.Ruiz", "Gilmore", "Betty Go",
-            "Cubao", "Anonas", "Katipunan", "Santolan", "Marikina", "Antipolo"]
-
         for position, label_text in zip(labelPositions, stationLabels):
             stationLabels = customtkinter.CTkLabel(sliderFrame, text=label_text, font=customFont2)
             stationLabels.place(relx=0.5, rely=position, anchor="center")
 
+        costLabel = customtkinter.CTkLabel(mainFrame, text="Fare Cost:", font=destinationFont)
+        costLabel.place(relx=0.5, rely=0.61, anchor="center")
+
+        costFrame = CTkFrame(mainFrame, height=50, width=250, corner_radius=15, border_color="#4a4d50")
+        costFrame.place(relx=0.5, rely=0.7, anchor="center")
+
 app = PageFormat()
 app.mainloop()
+
